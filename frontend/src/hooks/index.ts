ce72fa75c9cfabe4342b6bfa2fea848c30a73155
Aayush kumar <aayushkumarhigh@gmail.com>
2024-03-27 23:25:20 +0530

    some changes

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -5,7 +5,6 @@ export const useBlog = () => {
     const [allBlog, setAllBlog] = useState([]);
     useEffect(() => {
         setLoading(true)
-        console.log(import.meta.env.BACKEND_URL);
         axios.get(`${import.meta.env.BACKEND_URL}/api/v1/blog/all-bulk`)
             .then((res) => setAllBlog(res.data))
             .catch((e) => console.log(e))
