dacc427bbb25fdaf77588196e7c0148c0688301b
Aayush kumar <aayushkumarhigh@gmail.com>
2024-06-02 01:29:01 +0530

    done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ function App() {
           <Route path="/createblog" element={<CreateNewBlog />} />
           <Route path="/blog" element={<Blog />} />
         </Route>
+        <Route path="/" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
       </Routes>
